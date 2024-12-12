6e341e395ad741b33e9e5f98a192ceee6ecae862
jeid12 <niyokwizerajd123@gmail.com>
2024-12-12 10:41:33 +0200

    updated docker file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
+const app=document.getElementById('app');
+app.innerHTML=`<h1>We are going to build a bank app</h1>
 
+    <h2>Team members</h2>
+    <div class="member">
+        <h3>Kubwimana Jean d amour</h3>
+        <p>Role: Developer</p>
+    </div>
+    <div class="member">
+        <h3>Niyokwizera Jean d amour</h3>
+        <p>Role: Developer</p>
+    </div>
+    <div class="member">
+        <h3>Prosper Rukundo</h3>
+        <p>Role: Developer</p>
+    </div>
+    <div class="member">
+        <h3>Mugeri Frank</h3>
+        <p>Role: Developer</p>
+    </div>
+    <div class="member">
+        <h3>Valdmir</h3>
+        <p>Role: Developer</p>
+    </div>
+    <div class="member">
+        <h3>Ntwali Jamvier</h3>
+        <p>Role: Instructor</p>
+    </div>
+`;
